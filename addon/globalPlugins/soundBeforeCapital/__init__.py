6ab75d499c03bb56fa8c26f87c6208a015f65c44
Danstiv <50794055+Danstiv@users.noreply.github.com>
2022-10-02 23:49:35 +0700

    Initial commit

---
+++ addon/globalPlugins/soundBeforeCapital/__init__.py
@@ -0,0 +1,25 @@
+import os
+
+import globalPluginHandler
+from speech import commands, speech
+
+PATH_TO_SOUND = os.path.join(os.path.dirname(__file__), 'click.wav')
+
+
+class GlobalPlugin(globalPluginHandler.GlobalPlugin):
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.old_get_spelling_speech = speech.getSpellingSpeech
+        speech.getSpellingSpeech = self.decorator(speech.getSpellingSpeech)
+
+    def decorator(self, func):
+        def wrapper(*args, **kwargs):
+            for i in func(*args, **kwargs):
+                if isinstance(i, commands.BeepCommand) and i.hz==2000 and i.left==50 and i.right==50 and i.length==50:
+                    i = commands.WaveFileCommand(PATH_TO_SOUND)
+                yield i
+        return wrapper
+
+    def terminate(self):
+        speech.getSpellingSpeech = self.get_spelling_speech
---
+++ readme.md
@@ -0,0 +1,15 @@
+# Sound before capital
+
+An addon that replaces the beep that sounds when reading capital letters with a sound that is much more pleasant.
+
+I've been pissed off by this horrible high frequency beep for a very long time (several years), almost from the moment I heard it for the first time, and one day I decided to do something about it, the result was this addon.
+
+The add-on does not need to be configured, nor does it have its own gestures (hot keys).
+
+Simply install it and it will start working.
+
+## Changelog
+
+### 0.2.2
+
+Translated into English.
